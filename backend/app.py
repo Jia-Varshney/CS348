587f54d40fb023d5610c8eee5f53a79ff10f71aa
Jia Varshney <53506266+Jia-Varshney@users.noreply.github.com>
2026-05-01 20:46:52 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -85,7 +85,7 @@ def add_sighting():
         with db.session.begin(): 
             # backoff and retry strategy, user B will wait for user A to finish
             db.session.execute(text("PRAGMA busy_timeout = 5000"))          
-            db.session.execute(text('BEGIN IMMEDIATE')) # get exclusive lock  
+            db.session.execute(text('BEGIN IMMEDIATE')) # get write lock  
             if cryptid_id_raw == 'new':
                 # Check if the cryptid already exists in the database
                 existing_cryptid = Cryptid.query.filter_by(name=new_cryptid_name).first()
